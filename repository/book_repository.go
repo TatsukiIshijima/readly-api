d185d8e5716dd37a7260e1986a7ea577e53d51dc
TatsukiIshijima <tatsuki445@gmail.com>
2024-12-30 15:51:06 +0900

    refactor

--- repository/book_repository.go
+++ repository/book_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	db "readly/db/sqlc"
 	"time"
 )
@@ -21,7 +22,6 @@ func NewBookRepository(store *Store) BookRepository {
 
 type RegisterBookParams struct {
 	UserID        int64
-	BookID        int64
 	Title         string
 	Genres        []string
 	Description   string
@@ -33,32 +33,20 @@ type RegisterBookParams struct {
 	ISBN          string
 }
 
-func (repo BookRepositoryImpl) Register(ctx context.Context, args RegisterBookParams) error {
-	err := repo.store.execTx(ctx, func(q *db.Queries) error {
-		// 1. Author保存
-		_, err := q.CreateAuthor(ctx, args.AuthorName)
-		if err != nil {
+func (r BookRepositoryImpl) Register(ctx context.Context, args RegisterBookParams) error {
+	err := r.store.execTx(ctx, func(q *db.Queries) error {
+		if err := r.registerAuthorIfNotExist(ctx, q, args.AuthorName); err != nil {
 			return err
 		}
-		// 2. Publisher保存
-		_, err = q.CreatePublisher(ctx, args.PublisherName)
-		if err != nil {
+		if err := r.registerPublisherIfNotExist(ctx, q, args.PublisherName); err != nil {
 			return err
 		}
-		// 3. Genre保存（同時実行されるかも）
-		for _, name := range args.Genres {
-			_, err = q.GetGenreByName(ctx, name)
-			// すでに存在する場合はスキップ
-			if err == nil {
-				continue
-			}
-			_, err = q.CreateGenre(ctx, name)
-			if err != nil {
+		for _, genre := range args.Genres {
+			if err := r.registerGenreIfNotExist(ctx, q, genre); err != nil {
 				return err
 			}
 		}
-		// 4. Book保存
-		_, err = q.CreateBook(ctx, db.CreateBookParams{
+		book, err := q.CreateBook(ctx, db.CreateBookParams{
 			Title:         sql.NullString{String: args.Title, Valid: true},
 			Description:   sql.NullString{String: args.Description, Valid: true},
 			CoverImageUrl: sql.NullString{String: args.CoverImageURL, Valid: true},
@@ -68,29 +56,70 @@ func (repo BookRepositoryImpl) Register(ctx context.Context, args RegisterBookPa
 			PublishedDate: sql.NullTime{Time: args.PublishDate, Valid: true},
 			Isbn:          sql.NullString{String: args.ISBN, Valid: true},
 		})
-		// 5. BookGenre保存
-		for _, name := range args.Genres {
-			_, err = q.CreateBookGenre(ctx, db.CreateBookGenreParams{
-				BookID:    args.BookID,
-				GenreName: name,
-			})
-			if err != nil {
-				return err
-			}
-		}
-		// 6. ReadingHistory保存
-		_, err = q.CreateReadingHistory(ctx, db.CreateReadingHistoryParams{
-			UserID:    args.UserID,
-			BookID:    args.BookID,
-			Status:    db.ReadingStatusUnread,
-			StartDate: sql.NullTime{Time: time.Time{}, Valid: true},
-			EndDate:   sql.NullTime{Time: time.Time{}, Valid: false},
-		})
 		if err != nil {
 			return err
 		}
-
+		for _, genre := range args.Genres {
+			if _, err := q.CreateBookGenre(ctx, db.CreateBookGenreParams{
+				BookID:    book.ID,
+				GenreName: genre,
+			}); err != nil {
+				return err
+			}
+		}
+		if _, err := q.CreateReadingHistory(ctx, db.CreateReadingHistoryParams{
+			UserID:    args.UserID,
+			BookID:    book.ID,
+			Status:    db.ReadingStatusUnread,
+			StartDate: sql.NullTime{Time: time.Time{}, Valid: true},
+			EndDate:   sql.NullTime{Time: time.Time{}, Valid: false},
+		}); err != nil {
+			return err
+		}
 		return nil
 	})
 	return err
+}
+
+func (r BookRepositoryImpl) registerAuthorIfNotExist(ctx context.Context, q *db.Queries, name string) error {
+	var err error
+	_, err = q.GetAuthorByName(ctx, name)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			return err
+		}
+		_, err = q.CreateAuthor(ctx, name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r BookRepositoryImpl) registerPublisherIfNotExist(ctx context.Context, q *db.Queries, name string) error {
+	_, err := q.GetPublisherByName(ctx, name)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			return err
+		}
+		_, err = q.CreatePublisher(ctx, name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r BookRepositoryImpl) registerGenreIfNotExist(ctx context.Context, q *db.Queries, name string) error {
+	_, err := q.GetGenreByName(ctx, name)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			return err
+		}
+		_, err = q.CreateGenre(ctx, name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
