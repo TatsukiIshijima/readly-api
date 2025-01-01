8481f1a2f3f1074d8841b5453d207281e68131c5
TatsukiIshijima <tatsuki445@gmail.com>
2025-01-01 14:09:31 +0900

    create book api

---
+++ api/book_api.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"readly/domain"
+	"readly/repository"
+	"time"
+)
+
+type registerRequest struct {
+	UserID        int64     `json:"user_id" binding:"required"`
+	Title         string    `json:"title" binding:"required"`
+	Genres        []string  `json:"genres"`
+	Description   string    `json:"description"`
+	CoverImageURL string    `json:"cover_image_url"`
+	URL           string    `json:"url"`
+	AuthorName    string    `json:"author_name"`
+	PublisherName string    `json:"publisher_name"`
+	PublishDate   time.Time `json:"publish_date"`
+	ISBN          string    `json:"isbn"`
+}
+
+type registerResponse struct {
+	domain.Book
+}
+
+func (server *Server) registerBook(ctx *gin.Context) {
+	var req registerRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := repository.RegisterRequest{
+		UserID:        req.UserID,
+		Title:         req.Title,
+		Genres:        req.Genres,
+		Description:   req.Description,
+		CoverImageURL: req.CoverImageURL,
+		URL:           req.URL,
+		AuthorName:    req.AuthorName,
+		PublisherName: req.PublisherName,
+		PublishDate:   req.PublishDate,
+		ISBN:          req.ISBN,
+	}
+	book, err := server.bookRepo.Register(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	res := registerResponse{Book: book}
+	ctx.JSON(http.StatusOK, res)
+}
