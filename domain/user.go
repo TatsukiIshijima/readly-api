7c4ee8e3c5770437e2d84c06bd708ae8c4a1f7f8
TatsukiIshijima <tatsuki445@gmail.com>
2025-01-13 11:50:45 +0900

    move

--- domain/user.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type User struct {
-	ID       int64  `json:"id" binding:"required"`
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
