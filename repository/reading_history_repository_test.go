834c60ae7ce90521c54a98bc9bc0077b73a84659
TatsukiIshijima <tatsuki445@gmail.com>
2025-01-18 16:07:43 +0900

    create reading history repository test

---
+++ repository/reading_history_repository_test.go
@@ -0,0 +1,27 @@
+package repository
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+
+}
+
+func TestDelete(t *testing.T) {
+
+}
+
+func TestGetByUser(t *testing.T) {
+
+}
+
+func TestGetByUserAndBook(t *testing.T) {
+
+}
+
+func TestGetByUserAndStatus(t *testing.T) {
+
+}
+
+func TestUpdate(t *testing.T) {
+
+}
