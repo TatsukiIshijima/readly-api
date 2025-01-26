3c675405bbee1e5fa9b3da64b7fcab74c9e7ef8e
TatsukiIshijima <tatsuki445@gmail.com>
2025-01-26 14:58:11 +0900

    create sign up usecase

--- usecase/executor.go
+++ usecase/executor.go
@@ -6,6 +6,7 @@ import (
 )
 
 type Executor interface {
+	SignUp(ctx context.Context, req SignUpRequest) (*entity.User, error)
 	RegisterBook(ctx context.Context, req RegisterBookRequest) (*entity.Book, error)
 	DeleteBook(ctx context.Context, req DeleteBookRequest) error
 }
