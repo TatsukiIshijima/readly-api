2b9ec8a32f893d950feb875e5e2ef9f7632b1992
TatsukiIshijima <tatsuki445@gmail.com>
2025-01-26 16:49:25 +0900

    create user service

--- service/server.go
+++ service/server.go
@@ -6,15 +6,18 @@ import (
 
 type Server struct {
 	bookService BookService
+	userService UserService
 	router      *gin.Engine
 }
 
-func NewServer(bookService BookService) *Server {
-	server := &Server{bookService: bookService}
+func NewServer(bookService BookService, userService UserService) *Server {
+	server := &Server{bookService: bookService, userService: userService}
 	router := gin.Default()
 
 	router.POST("/books", server.bookService.Register)
 	router.DELETE("/books", server.bookService.Delete)
+	router.POST("/users/signup", server.userService.SignUp)
+	router.POST("/users/signin", server.userService.SignIn)
 
 	server.router = router
 	return server
