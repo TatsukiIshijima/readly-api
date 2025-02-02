51af38625664d430787d39565ff48c1cee21cd00
TatsukiIshijima <tatsuki445@gmail.com>
2025-02-02 11:04:31 +0900

    create auth middleware

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,54 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"readly/service/auth"
+	"strings"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationClaimKey   = "authorization_claim"
+)
+
+func Authorize(maker auth.TokenMaker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authorizationHeader := ctx.GetHeader(authorizationHeaderKey)
+		if authorizationHeader == "" {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) != 2 {
+			err := errors.New("invalid authorization header format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			err := fmt.Errorf("unsupported authorization type %s", authorizationType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+		accessToken := fields[1]
+		claims, err := maker.Verify(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authorizationClaimKey, claims)
+		ctx.Next()
+	}
+}
+
+// FIXME: この関数はcontroller/error.goにもあるので共通化したい
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
