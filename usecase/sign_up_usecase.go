7ae3cb0012c589e6b051d6c623208ac3dab22adf
TatsukiIshijima <tatsuki445@gmail.com>
2025-02-09 21:20:02 +0900

    return refresh token in sign up

--- usecase/sign_up_usecase.go
+++ usecase/sign_up_usecase.go
@@ -3,18 +3,36 @@ package usecase
 import (
 	"context"
 	"golang.org/x/crypto/bcrypt"
-	"readly/entity"
+	"readly/env"
 	"readly/repository"
+	"readly/service/auth"
 )
 
-type SignUpUseCase struct {
-	Executor
-	userRepo repository.UserRepository
+type SignUpUseCase interface {
+	SignUp(ctx context.Context, req SignUpRequest) (*SignUpResponse, error)
 }
 
-func NewSignUpUseCase(userRepo repository.UserRepository) SignUpUseCase {
-	return SignUpUseCase{
-		userRepo: userRepo,
+type SignUpUseCaseImpl struct {
+	config      env.Config
+	maker       auth.TokenMaker
+	transactor  repository.Transactor
+	sessionRepo repository.SessionRepository
+	userRepo    repository.UserRepository
+}
+
+func NewSignUpUseCase(
+	config env.Config,
+	maker auth.TokenMaker,
+	transactor repository.Transactor,
+	sessionRepo repository.SessionRepository,
+	userRepo repository.UserRepository,
+) SignUpUseCase {
+	return &SignUpUseCaseImpl{
+		config:      config,
+		maker:       maker,
+		transactor:  transactor,
+		sessionRepo: sessionRepo,
+		userRepo:    userRepo,
 	}
 }
 
@@ -24,24 +42,61 @@ type SignUpRequest struct {
 	Password string
 }
 
-func (u SignUpUseCase) SignUp(ctx context.Context, req SignUpRequest) (*entity.User, error) {
-	hashedPassword, err := generateHashedPassword(req.Password)
-	if err != nil {
-		return nil, handle(err)
-	}
-	user, err := u.userRepo.CreateUser(ctx, repository.CreateUserRequest{
-		Name:     req.Name,
-		Email:    req.Email,
-		Password: hashedPassword,
+type SignUpResponse struct {
+	AccessToken  string
+	RefreshToken string
+	UserID       int64
+	Name         string
+	Email        string
+}
+
+func (u *SignUpUseCaseImpl) SignUp(ctx context.Context, req SignUpRequest) (*SignUpResponse, error) {
+	var res *SignUpResponse
+	err := u.transactor.Exec(ctx, func() error {
+		hashedPassword, err := generateHashedPassword(req.Password)
+		if err != nil {
+			return err
+		}
+
+		user, err := u.userRepo.CreateUser(ctx, repository.CreateUserRequest{
+			Name:     req.Name,
+			Email:    req.Email,
+			Password: hashedPassword,
+		})
+		if err != nil {
+			return err
+		}
+
+		accessTokenPayload, err := u.maker.Generate(user.ID, u.config.AccessTokenDuration)
+		if err != nil {
+			return err
+		}
+
+		refreshTokenPayload, err := u.maker.Generate(user.ID, u.config.RefreshTokenDuration)
+		if err != nil {
+			return err
+		}
+
+		sessionReq := repository.CreateSessionRequest{
+			UserID:       user.ID,
+			RefreshToken: refreshTokenPayload.Token,
+			ExpiresAt:    refreshTokenPayload.ExpiredAt,
+		}
+		err = u.sessionRepo.CreateSession(ctx, sessionReq)
+		if err != nil {
+			return err
+		}
+
+		res = &SignUpResponse{
+			AccessToken:  accessTokenPayload.Token,
+			RefreshToken: refreshTokenPayload.Token,
+			UserID:       user.ID,
+			Name:         user.Name,
+			Email:        user.Email,
+		}
+		return nil
 	})
-	if err != nil {
-		return nil, handle(err)
-	}
-	return &entity.User{
-		ID:    user.ID,
-		Name:  user.Name,
-		Email: user.Email,
-	}, nil
+	return res, handle(err)
 }
 
 func generateHashedPassword(password string) (string, error) {
