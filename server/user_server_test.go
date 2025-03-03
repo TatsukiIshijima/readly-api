2b1f372f0f45b04fd3af93c3b374e57a7fe6ce61
TatsukiIshijima <tatsuki445@gmail.com>
2025-03-03 21:42:24 +0900

    add sign up test case

--- server/user_server_test.go
+++ server/user_server_test.go
@@ -3,8 +3,10 @@
 package server
 
 import (
+	"context"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 	sqlc "readly/db/sqlc"
 	"readly/env"
@@ -48,15 +50,25 @@ func NewTestUserServer(t *testing.T) *UserServerImpl {
 
 func TestSignUp(t *testing.T) {
 	us := NewTestUserServer(t)
+	ctx := metadata.NewIncomingContext(
+		context.Background(),
+		NewTestMetadata(
+			"grpc-node-js/1.11.0-postman.1",
+			"",
+			"127.0.0.1",
+		),
+	)
 
 	testCases := []struct {
-		name  string
-		req   *pb.SignUpRequest
-		check func(t *testing.T, res *pb.SignUpResponse, err error)
+		name    string
+		prepare func(t *testing.T)
+		req     *pb.SignUpRequest
+		check   func(t *testing.T, res *pb.SignUpResponse, err error)
 	}{
 		{
-			name: "invalid request by missing required fields",
-			req:  &pb.SignUpRequest{},
+			name:    "invalid request by missing required fields",
+			prepare: func(t *testing.T) {},
+			req:     &pb.SignUpRequest{},
 			check: func(t *testing.T, res *pb.SignUpResponse, err error) {
 				require.Nil(t, res)
 				require.Error(t, err)
@@ -67,10 +79,13 @@ func TestSignUp(t *testing.T) {
 		},
 		{
 			name: "invalid request by empty name",
+			prepare: func(t *testing.T) {
+
+			},
 			req: &pb.SignUpRequest{
 				Name:     "",
 				Email:    testdata.RandomEmail(),
-				Password: testdata.RandomString(16),
+				Password: "1234abcD@",
 			},
 			check: func(t *testing.T, res *pb.SignUpResponse, err error) {
 				require.Nil(t, res)
@@ -80,11 +95,91 @@ func TestSignUp(t *testing.T) {
 				require.Equal(t, codes.InvalidArgument, s.Code())
 			},
 		},
+		{
+			name: "invalid request by invalid email",
+			prepare: func(t *testing.T) {
+
+			},
+			req: &pb.SignUpRequest{
+				Name:     "TestUser",
+				Email:    "invalid",
+				Password: "1234abcD@",
+			},
+			check: func(t *testing.T, res *pb.SignUpResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				s, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, s.Code())
+			},
+		},
+		{
+			name: "invalid request by short password",
+			prepare: func(t *testing.T) {
+
+			},
+			req: &pb.SignUpRequest{
+				Name:     testdata.RandomString(10),
+				Email:    testdata.RandomEmail(),
+				Password: "short",
+			},
+			check: func(t *testing.T, res *pb.SignUpResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				s, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, s.Code())
+			},
+		},
+		{
+			name: "invalid request by duplicate email",
+			prepare: func(t *testing.T) {
+				req := &pb.SignUpRequest{
+					Name:     "TestUser",
+					Email:    "duplicate@example.com",
+					Password: "1234abcD@",
+				}
+				_, err := us.SignUp(ctx, req)
+				require.NoError(t, err)
+			},
+			req: &pb.SignUpRequest{
+				Name:     "TestUser",
+				Email:    "duplicate@example.com",
+				Password: "1234abcD@",
+			},
+			check: func(t *testing.T, res *pb.SignUpResponse, err error) {
+				require.Nil(t, res)
+				require.Error(t, err)
+				s, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.AlreadyExists, s.Code())
+			},
+		},
+		{
+			name: "success: valid request",
+			prepare: func(t *testing.T) {
+
+			},
+			req: &pb.SignUpRequest{
+				Name:     "TestUser",
+				Email:    testdata.RandomEmail(),
+				Password: "1234abcD@",
+			},
+			check: func(t *testing.T, res *pb.SignUpResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.NotEmpty(t, res.GetAccessToken())
+				require.NotEmpty(t, res.GetRefreshToken())
+				require.NotEmpty(t, res.GetUserId())
+				require.Equal(t, "TestUser", res.GetName())
+			},
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			res, err := us.SignUp(nil, tc.req)
+			tc.prepare(t)
+			res, err := us.SignUp(ctx, tc.req)
 			tc.check(t, res, err)
 		})
 	}
