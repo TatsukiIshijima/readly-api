8812d95309697e7a8ccdada96ed222c3ab9e7c2b
TatsukiIshijima <tatsuki445@gmail.com>
2025-05-04 13:01:30 +0900

    move method

--- entity/reading_status.go
+++ entity/reading_status.go
@@ -1,5 +1,7 @@
 package entity
 
+import pb "readly/pb/readly/v1"
+
 type ReadingStatus int
 
 const (
@@ -8,3 +10,16 @@ const (
 	Done
 	Unknown
 )
+
+func NewReadingStatusFromProto(proto pb.ReadingStatus) ReadingStatus {
+	switch proto {
+	case pb.ReadingStatus_UNREAD:
+		return Unread
+	case pb.ReadingStatus_READING:
+		return Reading
+	case pb.ReadingStatus_DONE:
+		return Done
+	default:
+		return Unknown
+	}
+}
