78b500e144ff419acb14e11a6deff6e8e60509df
TatsukiIshijima <tatsuki445@gmail.com>
2025-05-09 23:03:50 +0900

    rename

--- usecase/get_book_list_request.go
+++ usecase/get_book_list_request.go
@@ -4,22 +4,22 @@ import "readly/repository"
 
 type GetBookListRequest struct {
 	UserID int64
-	limit  int32
-	offset int32
+	Limit  int32
+	Offset int32
 }
 
 func NewGetBookListRequest(userID int64, limit, offset int32) GetBookListRequest {
 	return GetBookListRequest{
 		UserID: userID,
-		limit:  limit,
-		offset: offset,
+		Limit:  limit,
+		Offset: offset,
 	}
 }
 
 func (r GetBookListRequest) ToRepoRequest() repository.GetReadingHistoryByUserRequest {
 	return repository.GetReadingHistoryByUserRequest{
 		UserID: r.UserID,
-		Limit:  r.limit,
-		Offset: r.offset,
+		Limit:  r.Limit,
+		Offset: r.Offset,
 	}
 }
