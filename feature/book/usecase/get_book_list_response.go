b23c41a0e82494c21a8f6305530f7f7aedeaea1d
TatsukiIshijima <tatsuki445@gmail.com>
2025-07-09 21:45:37 +0900

    add mapper

--- feature/book/usecase/get_book_list_response.go
+++ feature/book/usecase/get_book_list_response.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "readly/feature/book/domain"
+import (
+	"readly/feature/book/domain"
+	pb "readly/pb/readly/v1"
+)
 
 type GetBookListResponse struct {
 	Books []domain.Book
@@ -9,5 +12,29 @@ type GetBookListResponse struct {
 func NewGetBookListResponse(books []domain.Book) *GetBookListResponse {
 	return &GetBookListResponse{
 		Books: books,
+	}
+}
+
+func (r *GetBookListResponse) ToProto() *pb.GetBookListResponse {
+	pbBooks := make([]*pb.Book, len(r.Books))
+	for i, book := range r.Books {
+		pbBooks[i] = &pb.Book{
+			Id:            book.ID,
+			Title:         book.Title,
+			Genres:        book.Genres,
+			Description:   book.Description,
+			CoverImageUrl: book.CoverImageURL,
+			Url:           book.URL,
+			AuthorName:    book.AuthorName,
+			PublisherName: book.PublisherName,
+			PublishDate:   book.PublishDate.ToProto(),
+			Isbn:          book.ISBN,
+			ReadingStatus: pb.ReadingStatus(book.Status),
+			StartDate:     book.StartDate.ToProto(),
+			EndDate:       book.EndDate.ToProto(),
+		}
+	}
+	return &pb.GetBookListResponse{
+		Books: pbBooks,
 	}
 }
