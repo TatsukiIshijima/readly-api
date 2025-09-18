aa35579275730aba6a8df0968041948472070cd3
TatsukiIshijima <tatsuki445@gmail.com>
2025-09-18 22:17:14 +0900

    check nil

--- feature/book/usecase/register_book_response.go
+++ feature/book/usecase/register_book_response.go
@@ -19,6 +19,19 @@ func NewRegisterBookResponse(book *domain.Book) *RegisterBookResponse {
 }
 
 func (r *RegisterBookResponse) ToProto() *pb.RegisterBookResponse {
+	var publishDate *pb.Date
+	var startDate *pb.Date
+	var endDate *pb.Date
+	if r.Book.PublishDate != nil {
+		publishDate = r.Book.PublishDate.ToProto()
+	}
+
+	if r.Book.StartDate != nil {
+		startDate = r.Book.StartDate.ToProto()
+	}
+	if r.Book.EndDate != nil {
+		endDate = r.Book.EndDate.ToProto()
+	}
 	return &pb.RegisterBookResponse{
 		Book: &pb.Book{
 			Id:            r.Book.ID,
@@ -29,11 +42,11 @@ func (r *RegisterBookResponse) ToProto() *pb.RegisterBookResponse {
 			Url:           r.Book.URL,
 			AuthorName:    r.Book.AuthorName,
 			PublisherName: r.Book.PublisherName,
-			PublishDate:   r.Book.PublishDate.ToProto(),
+			PublishDate:   publishDate,
 			Isbn:          r.Book.ISBN,
 			ReadingStatus: pb.ReadingStatus(r.Book.Status),
-			StartDate:     r.Book.StartDate.ToProto(),
-			EndDate:       r.Book.EndDate.ToProto(),
+			StartDate:     startDate,
+			EndDate:       endDate,
 		},
 	}
 }
